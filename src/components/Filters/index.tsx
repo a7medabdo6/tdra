86ea4c412b248f7d9c56173fd5029e1eca2f4185
abbass <abbas6049@gmail.com>
2024-04-12 13:13:44 +0200

    responsive

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import React, { useState } from "react";
 import CircleIcon from "@mui/icons-material/Circle";
 import FilterListIcon from "@mui/icons-material/FilterList";
@@ -85,190 +85,206 @@ function FilterMonth({
   };
   return (
     <Box className="d-flex justify-content-between" sx={{ marginLeft: "40px" }}>
-      <Typography sx={{ fontWeight: "bold" }}>{title}</Typography>
-
-      {/* Communication Line Chart */}
-      <Box className="d-flex month_filters">
-        {showMonths && (
-          <>
-            {" "}
-            <Typography
-              onClick={() => handleMonthClicked("1m")}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "20px",
-                cursor: "pointer",
-                color: monthClicked == "1m" ? "#9D805F" : "unset",
-              }}
-            >
-              1M
-            </Typography>
-            <Typography
-              onClick={() => handleMonthClicked("6m")}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "20px",
-                cursor: "pointer",
-                color: monthClicked == "6m" ? "#9D805F" : "unset",
-              }}
-            >
-              6M
-            </Typography>
-            <Typography
-              onClick={() => handleMonthClicked("1y")}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "20px",
-                cursor: "pointer",
-                color: monthClicked == "1y" ? "#9D805F" : "unset",
-              }}
-            >
-              1Y
-            </Typography>
-            <Typography
-              onClick={() => handleMonthClicked("5y")}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "20px",
-                cursor: "pointer",
-                color: monthClicked == "5y" ? "#9D805F" : "unset",
-              }}
-            >
-              5Y
-            </Typography>
-            <Typography
-              onClick={() => handleMonthClicked("max")}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "20px",
-                cursor: "pointer",
-                color: monthClicked == "max" ? "#9D805F" : "unset",
-              }}
-            >
-              MAX
-            </Typography>
-          </>
-        )}
-
-        <Box className="d-flex">
-          <Box className="fitler_status d-flex">
-            {" "}
-            <CircleIcon
-              sx={{ color: "#9D805F", fontSize: "15px", marginInline: "5px" }}
-            />{" "}
-            Success
-          </Box>
-          <Box className="fitler_status d-flex">
-            {" "}
-            <CircleIcon
-              sx={{ color: "#354B62", fontSize: "15px", marginInline: "5px" }}
-            />{" "}
-            Failure
-          </Box>
-        </Box>
-        {showFilterBtn && (
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          <Typography sx={{ fontWeight: "bold" }}>{title}</Typography>
+        </Grid>
+        <Grid item xs={12} md={8} sx={{ display: { xs: "none", sm: "block" } }}>
+          {/* Communication Line Chart */}
           <Box
-            className="w-100"
-            sx={{
-              width: "100%",
-              boxShadow: "unset",
-              position: "relative",
-              overflow: "visible",
-            }}
+            className="d-flex month_filters"
+            sx={{ justifyContent: "flex-end", marginRight: "30px" }}
           >
-            <BasicButton
-              style={{
-                //   position: "absolute",
-                //   right: "10px",
-                //   top: "-40px",
-                width: "150px",
-              }}
-              text={
-                <Box
+            {showMonths && (
+              <>
+                {" "}
+                <Typography
+                  onClick={() => handleMonthClicked("1m")}
                   sx={{
                     display: "flex",
-                    justifyContent: "space-between",
                     alignItems: "center",
-                    width: "100%",
+                    height: "20px",
+                    cursor: "pointer",
+                    color: monthClicked == "1m" ? "#9D805F" : "unset",
                   }}
                 >
-                  <FilterListIcon />
-                  <Typography>Filter</Typography> <KeyboardArrowDownIcon />{" "}
-                </Box>
-              }
-              bgColor={COLORS.secondary}
-              textColor={COLORS.white}
-              onClick={handleShowFilters}
-            />
-            {showFilters && (
+                  1M
+                </Typography>
+                <Typography
+                  onClick={() => handleMonthClicked("6m")}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "20px",
+                    cursor: "pointer",
+                    color: monthClicked == "6m" ? "#9D805F" : "unset",
+                  }}
+                >
+                  6M
+                </Typography>
+                <Typography
+                  onClick={() => handleMonthClicked("1y")}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "20px",
+                    cursor: "pointer",
+                    color: monthClicked == "1y" ? "#9D805F" : "unset",
+                  }}
+                >
+                  1Y
+                </Typography>
+                <Typography
+                  onClick={() => handleMonthClicked("5y")}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "20px",
+                    cursor: "pointer",
+                    color: monthClicked == "5y" ? "#9D805F" : "unset",
+                  }}
+                >
+                  5Y
+                </Typography>
+                <Typography
+                  onClick={() => handleMonthClicked("max")}
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "20px",
+                    cursor: "pointer",
+                    color: monthClicked == "max" ? "#9D805F" : "unset",
+                  }}
+                >
+                  MAX
+                </Typography>
+              </>
+            )}
+
+            <Box className="d-flex">
+              <Box className="fitler_status d-flex">
+                {" "}
+                <CircleIcon
+                  sx={{
+                    color: "#9D805F",
+                    fontSize: "15px",
+                    marginInline: "5px",
+                  }}
+                />{" "}
+                Success
+              </Box>
+              <Box className="fitler_status d-flex">
+                {" "}
+                <CircleIcon
+                  sx={{
+                    color: "#354B62",
+                    fontSize: "15px",
+                    marginInline: "5px",
+                  }}
+                />{" "}
+                Failure
+              </Box>
+            </Box>
+            {showFilterBtn && (
               <Box
                 sx={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                  position: "absolute",
-                  right: "10px",
-                  top: "60px",
-                  width: "350px",
-                  flexDirection: "column",
-                  backgroundColor: "white",
-                  padding: "20px",
-                  borderRadius: "10px",
+                  boxShadow: "unset",
+                  position: "relative",
+                  overflow: "visible",
                 }}
               >
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    marginBottom: "20px",
+                <BasicButton
+                  style={{
+                    //   position: "absolute",
+                    //   right: "10px",
+                    //   top: "-40px",
+                    width: "150px",
                   }}
-                >
-                  <DateInput
-                    initValue={filters.fromDate}
-                    text="From: "
-                    onChange={setFilters}
-                    name={"fromDate"}
-                    bgColor={COLORS.secondary}
-                    textColor={COLORS.white}
-                    style={{ padding: "5px 7px" }}
-                  />
-
-                  <DateInput
-                    text="To: "
-                    onChange={setFilters}
-                    name={"toDate"}
-                    bgColor={COLORS.secondary}
-                    textColor={COLORS.white}
-                    style={{ padding: "5px 7px" }}
-                  />
-                </Box>
-                {showSearchInput && (
-                  <SearchInput
-                    style={{
-                      backgroundColor: COLORS.secondary,
-                      color: COLORS.white,
-
-                      "& .MuiOutlinedInput-root": {
-                        color: COLORS.white,
-                      },
-                      "& .MuiInputBase-input": {
-                        padding: "10px",
-                      },
+                  text={
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        width: "100%",
+                      }}
+                    >
+                      <FilterListIcon />
+                      <Typography>
+                        Filter
+                      </Typography> <KeyboardArrowDownIcon />{" "}
+                    </Box>
+                  }
+                  bgColor={COLORS.secondary}
+                  textColor={COLORS.white}
+                  onClick={handleShowFilters}
+                />
+                {showFilters && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "flex-start",
+                      alignItems: "center",
+                      position: "absolute",
+                      right: "10px",
+                      top: "60px",
+                      width: "350px",
+                      flexDirection: "column",
+                      backgroundColor: "white",
+                      padding: "20px",
+                      borderRadius: "10px",
                     }}
-                    onchange={debouncedOnChange}
-                  />
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "flex-start",
+                        alignItems: "center",
+                        marginBottom: "20px",
+                      }}
+                    >
+                      <DateInput
+                        initValue={filters.fromDate}
+                        text="From: "
+                        onChange={setFilters}
+                        name={"fromDate"}
+                        bgColor={COLORS.secondary}
+                        textColor={COLORS.white}
+                        style={{ padding: "5px 7px" }}
+                      />
+
+                      <DateInput
+                        text="To: "
+                        onChange={setFilters}
+                        name={"toDate"}
+                        bgColor={COLORS.secondary}
+                        textColor={COLORS.white}
+                        style={{ padding: "5px 7px" }}
+                      />
+                    </Box>
+                    {showSearchInput && (
+                      <SearchInput
+                        style={{
+                          backgroundColor: COLORS.secondary,
+                          color: COLORS.white,
+
+                          "& .MuiOutlinedInput-root": {
+                            color: COLORS.white,
+                          },
+                          "& .MuiInputBase-input": {
+                            padding: "10px",
+                          },
+                        }}
+                        onchange={debouncedOnChange}
+                      />
+                    )}
+                  </Box>
                 )}
               </Box>
             )}
           </Box>
-        )}
-      </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
